1cfc534e4d4fa01eeb1fd06cb8af33aba4bb86bd
Taesun <liamtsy@icloud.com>
2022-08-11 13:34:00 +0900

    Update index.js

--- momentum/assignments/assignment4/index.js
+++ momentum/assignments/assignment4/index.js
@@ -1,23 +1,26 @@
 const countdown = document.querySelector('h2#countdown');
 function getCountdown() {
-	const ChristmasEve = new Date(
-		'December 24, 2022 00:00:00'
-	).getTime();
-	const now = new Date().getTime();
-	const distance = ChristmasEve - now;
-	const days = String(
-		Math.floor(distance / (1000 * 60 * 60 * 24))
-	).padStart(2, '0');
-	const hours = String(
-		Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-	).padStart(2, '0');
-	const minutes = String(
-		Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-	).padStart(2, '0');
-	const seconds = String(
-		Math.floor((distance % (1000 * 60)) / 1000)
-	).padStart(2, '0');
-	countdown.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+	const ChristmasEve = new Date('2022-12-24');
+	const now = new Date();
+	const difference = ChristmasEve - now;
+	const secondsInMs = Math.floor(difference / 1000);
+	const minutesInMs = Math.floor(secondsInMs / 60);
+	const hoursInMs = Math.floor(minutesInMs / 60);
+	const days = Math.floor(hoursInMs / 24);
+	// 1. padStart() way
+	const secondsStr = String(secondsInMs % 60).padStart(2, '0');
+	const minutesStr = String(minutesInMs % 60).padStart(2, '0');
+	const hoursStr = String(hoursInMs % 24).padStart(2, '0');
+	const daysStr = String(days).padStart(2, '0');
+	// 2. ternary operator way
+	// const seconds = secondsInMs % 60;
+	// const minutes = minutesInMs % 60;
+	// const hours = hoursInMs % 24;
+	// const daysStr = `${days < 10 ? `0${days}` : days}d`;
+	// const hoursStr = `${hours < 10 ? `0${hours}` : hours}h`;
+	// const minutesStr = `${minutes < 10 ? `0${minutes}` : minutes}m `;
+	// const secondsStr = `${seconds < 10 ? `0${seconds}` : seconds}s`;
+	countdown.innerHTML = `${daysStr}d ${hoursStr}h ${minutesStr}m ${secondsStr}s`;
 }
 
 getCountdown();
