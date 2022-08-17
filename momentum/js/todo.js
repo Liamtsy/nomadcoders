b3d1a783658cc4b38cbdb18680007914e362d383
Taesun <liamtsy@icloud.com>
2022-08-17 23:53:12 +0900

    Added comments

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -7,19 +7,32 @@ let toDos = []; // Array나 Object 자료형은 const로 선언해도 내부의 
 // 참조 자료형은 그 자료의 주소를 들고 있는 겁니다. const a = [1,2,3,4]; 하면 a는 [1,2,3,4]가 들어있는 배열의 시작지점 주소를 들고 있는 거예여. 이 상태에서 배열에 5가 추가 되던 6이 추가되던 a가 시작지점의 주소를 쥐고 있는 건 변하지 않는 상태에서 배열의 내용물만 달라지는 거예여. 다만 이때 a에 들어있는 주소를 바꾸려고 한다? 이것은 오류가 납니다. 말씀하신대로 const로 즉 상수로 선언했기 때문이죠.
 //  arr = { name : "Kim" } 같이 자료형 자체를 새로 할당하는것은 const 특성상 되지 않습니다..
 function saveToDos() {
-	localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); // Make a toDos array to a string.
+	localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); // Make a toDos array to a string. Becuase localStorage only accepts strings.
 }
 
 function deleteToDo(event) {
 	const li = event.target.parentElement; // event.target is the button clicked.
 	li.remove();
-	toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+	// li는 삭제 됐으나, localStorage에는 toDos의 아이템이 삭제되지 않음.
+	// li는 UI에서 삭제됐으나 우리는 여전히 변수에 접근이 가능함. 그래서 li.id 참조 가능
+	// 그렇기 때문에 filter()를 사용하여 삭제해야함. 선택옵션이라고 보면 편함. forEach처럼 요소 하나씩 다 건드림. 요소 개수만큼 함수가 호출되는 것.
+	// 선택옵션으로 제외된 아이템을 제외한 후, New Array를 줌. 기존 array를 변경해주는게 아님!!!
+	// How to use filter() ?
+
+	// const life = ["희망", "불안", "행복", "비꼬는 마음"]
+	// function goodLife(item){return item !== "비꼬는 마음"}
+	// life.filter(goodLife) // ["희망", "불안", "행복"] (결과값)
+	// 아래는 Arrow fucntion 이용함.
+	toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); // != 사용하면 형변환 안 해도 됨. 그러나 권장X.
+	// toDos item의 id가 li.id와 같지 않으면 새 배열에 추가(True). 같으면 추가 X(False).
+	// 그래서 클릭한 버튼의 parent인 li의 아이디와 배열의 모든 요소를 하나씩 비교해서, 같은 아이디를 가진 요소를 찾아서 걔만 제외해주는 것임.
 	saveToDos();
 }
 
 function paintToDo(newToDo) {
 	const li = document.createElement('li');
-	li.id = newToDo.id;
+	li.id = newToDo.id; // Give each li a unique id.
+	//newTodo.id는 number이지만 li.id는 DOM을 직접 건드린거라 string으로 형변환해서 받아들임.
 	const span = document.createElement('span');
 	span.innerText = newToDo.text;
 	const button = document.createElement('button');
@@ -38,7 +51,7 @@ function handleToDoSubmit(event) {
 	toDoInput.value = '';
 	const newTodoObj = {
 		text: newToDo,
-		id: Date.now()
+		id: Date.now() // This is for giving a unique id to each newToDo. Date.now() is a function that returns the current time in milliseconds.
 	};
 	toDos.push(newTodoObj);
 	paintToDo(newTodoObj);
@@ -50,7 +63,8 @@ toDoForm.addEventListener('submit', handleToDoSubmit);
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
-	const parsedToDos = JSON.parse(savedToDos);
-	toDos = parsedToDos;
+	//if (savedToDos) {}
+	const parsedToDos = JSON.parse(savedToDos); // Make a string to an array.
+	toDos = parsedToDos; // toDos에게 이전 저장된 데이터를 기억하게 하기 위해서 업데이트 해줘여함.
 	parsedToDos.forEach(paintToDo);
 }
--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -3,11 +3,12 @@ const city = document.querySelector('#weather span:last-child');
 const API_KEY = '4cb2ff8508ad4fc946d2446cc14b2b02';
 
 function onGeoOk(position) {
+	//success 함수는 GeolocationPosition object를 parameter로 get함.
 	const lat = position.coords.latitude;
 	const lon = position.coords.longitude;
 	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-	fetch(url)
-		.then((response) => response.json())
+	fetch(url) // 해당 url에 갈 필요 없이 js가 fetch를 해서 응답을 받음.
+		.then((response) => response.json()) // fetch 후에 then 안의 것을 실행. 어려우니까 유튜브 클론코딩 고고
 		.then((data) => {
 			city.innerText = data.name;
 			weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
